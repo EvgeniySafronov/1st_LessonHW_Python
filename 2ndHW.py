545cfed7434d577f4f038c155ca662dbed72fcde
E.Safronov <madjuster-io@yandex.ru>
2022-09-05 15:54:59 +1000

    Добавлена вторая задача

---
+++ 2ndHW.py
@@ -0,0 +1,24 @@
+"""Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат."""
+
+
+def entered_num(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def check(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = entered_num(3)
+
+if check(statement) == True:
+    print('Утверждение истинно')
+else:
+    print('Утверждение ложно')
